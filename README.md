c9a5ee2122d35256f7a2907fa62e96c2e298183d
Abdulrahman Jamjoom <abdulrahmanjamjoom15@kingsacademy.edu.jo>
2014-07-04 21:46:37 -0400

    created a new images file with the images of the password generator strength and added it to the website and centrilized it using the style.css sheet, added the basic structure of the index.php sheet

---
+++ README.md
@@ -0,0 +1,11 @@
+#xkcd Password Generator
+
+##Live URL
+
+##Description
+A simpe program that generates random xkcd style passwords.
+
+##Details for teaching team
+No login required
+
+##Outside code
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>xkcd Password Generator</title>
+	
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<link rel='stylesheet' href='style.css' type='text/css'>
+	<?php require 'logic.php'; ?>
+
+
+
+</head>
+<body>
+<h1>xkcd Password Generator</h1>
+<img src = "images/password_strength.png" alt = "sketch of password strength in a xkcd password generator">
+</body>
+</html>
