c8bcd66668c79c94bf4faf4b7d898cb50adf67b4
Abdulrahman Jamjoom <abdulrahmanjamjoom15@kingsacademy.edu.jo>
2014-07-04 22:27:16 -0400

    added the form and th e options for the password, centralized everything in the style.css sheet

--- index.php
+++ index.php
@@ -14,6 +14,25 @@
 </head>
 <body>
 <h1>xkcd Password Generator</h1>
+
+<form action='index.php' method='POST'>
+	<p class = 'options'>
+
+				<label for='number_of_words'>Number of Words:</label>
+				<input maxlength=10 type='text' name='number_of_words' id='number_of_words' value=''>  (Max 10)
+				<br>
+					
+				<input type='checkbox' name='add_number' id='add_number' > 
+				<label for='add_number'>Add a number</label>
+				<br>
+
+				<input type='checkbox' name='add_symbol' id='add_symbol' > 
+				<label for='add_symbol'>Add a symbol</label>
+	</p>
+		
+				<input type='submit' value='Generate Password'>
+</form>
+
 <img src = "images/password_strength.png" alt = "sketch of password strength in a xkcd password generator">
 </body>
 </html>
